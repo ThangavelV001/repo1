21e1cf3724f61b24d0e5e3f70d6747cec16e2255
Thangavel V <76422033+ThangavelV001@users.noreply.github.com>
2024-09-16 07:33:07 +0530

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,10 +1,7 @@
 print("hello world")
 
-<<<<<<< HEAD
 #comment
-=======
 #added comment
 #added new changes in local
 #print("hello world")
 #commented
->>>>>>> 18a9735ec76e022cf210fc480703257c558499fd
